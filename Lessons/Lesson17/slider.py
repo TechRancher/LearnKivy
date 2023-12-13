11b738cf28319b8eb960007c24fa83ea02fa3de5
TechRancher <sikes.jay@gmail.com>
2023-12-12 23:09:45 -0600

    Added new Lesson

--- Lessons/Lesson17/slider.py
+++ Lessons/Lesson17/slider.py
@@ -8,12 +8,18 @@ from kivymd.app import MDApp
 Builder.load_file("slider.kv")
 
 class MyLayout(Widget):
-    pass
+    # Create slide_it function
+    def slide_it(self, *args):
+        # Set the text to the value of the slider
+        self.slide_text.text = str(int(args[1]))
+        # Set the text size to the value of the slider * 2
+        self.slide_text.font_size = str(int(args[1]) * 2)
 
-class AwesomeApp(MDApp):
+class TheSliderApp(MDApp):
     def build(self):
-        Window.clearcolor = (.2,.2,.2,1)
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
         return MyLayout()
     
 if __name__ == "__main__":
-    AwesomeApp().run()
+    TheSliderApp().run()
