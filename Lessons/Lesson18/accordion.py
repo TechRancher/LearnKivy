d1154515e9d427ddbaf7bbb84719287b4d7a5473
TechRancher <sikes.jay@gmail.com>
2023-12-14 10:09:34 -0600

    Added more Lessons

---
+++ Lessons/Lesson18/accordion.py
@@ -0,0 +1,20 @@
+from kivy.app import App 
+from kivy.uix.widget import Widget 
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder 
+from kivy.core.window import Window 
+from kivymd.app import MDApp 
+
+Builder.load_file("accordion.kv")
+
+class MyLayout(Widget):
+    pass
+
+class TheSliderApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return MyLayout()
+    
+if __name__ == "__main__":
+    TheSliderApp().run()
---
+++ Lessons/Lesson20/checkBox.py
@@ -0,0 +1,47 @@
+from kivy.app import App 
+from kivy.uix.widget import Widget 
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder 
+from kivy.core.window import Window 
+from kivymd.app import MDApp 
+
+Builder.load_file("checkBox.kv")
+
+class MyLayout(Widget):
+    # Create a list to hold the toppings
+    checks = []
+    # Create a checkbox function
+    def checkbox_click(self, instance, value, Topping):
+        # Check if the checkbox is checked
+        if value is True:
+            # Append the topping to the list
+            MyLayout.checks.append(Topping)
+            # Create a string to hold the toppings
+            tops = ''
+            # Loop through the list of toppings and add them to the string
+            for check in MyLayout.checks:
+                tops = f"{tops} {check},"
+            # Update the label
+            self.ids.output_label.text = f"Your Selection: {tops}"
+        else:
+            # Remove the topping from the list
+            MyLayout.checks.remove(Topping)
+            # Create a string to hold the toppings
+            tops = ''
+            # Loop through the list of toppings and add them to the string
+            for check in MyLayout.checks:
+                tops = f"{tops} {check},"
+            # Update the label
+            self.ids.output_label.text = f"Your Selection: {tops}"
+            # If there is no toppings selected, clear the label
+            if tops == '':
+                self.ids.output_label.text = ""
+
+class TheSliderApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return MyLayout()
+    
+if __name__ == "__main__":
+    TheSliderApp().run()
